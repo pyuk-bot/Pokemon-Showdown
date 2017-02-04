199e11ca975ad1b3238846d6853449cfa9bcf1fe
MacChaeger <msaimrkon@gmail.com>
2017-02-03 22:05:58 -0600

    Update formats-data.js

--- data/formats-data.js
+++ data/formats-data.js
@@ -2190,7 +2190,7 @@ exports.BattleFormatsData = {
 			{"generation": 5, "level": 30, "gender": "M", "abilities":["owntempo"], "moves":["outrage", "magiccoat"]},
 			{"generation": 5, "level": 99, "gender": "M", "abilities":["owntempo"], "moves":["nastyplot", "sheercold", "attract", "shadowball"]},
 		],
-		gen: 5
+		gen: 5,
 		tier: "Illegal",
 	},
 	miltank: {
