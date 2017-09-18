1b83cdca47783fe9a3e27dd552ad927bc023a925
MacChaeger <msaimrkon@gmail.com>
2017-09-18 18:23:59 -0500

    Double negation?

--- data/scripts.js
+++ data/scripts.js
@@ -90,7 +90,7 @@ exports.BattleScripts = {
 		this.runEvent('AfterMove', pokemon, target, move);
 
 		// Dancer's activation order is completely different from any other event, so it's handled separately
-		if (move.flags['dance']) {
+		if (!!move.flags['dance']) {
 			let dancers = [];
 			for (let i = 0; i < this.sides.length; i++) {
 				for (let j = 0; j < this.sides[i].active.length; j++) {
