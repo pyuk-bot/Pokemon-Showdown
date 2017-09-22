57f5418a59676dc998723487a4340e3de5e20b7f
MacChaeger <msaimrkon@gmail.com>
2017-09-21 23:54:31 -0500

    test 14

--- data/scripts.js
+++ data/scripts.js
@@ -106,7 +106,8 @@ exports.BattleScripts = {
 			if (trickRoom) delete this.pseudoWeather['trickroom'];
 			dancers.sort(this.comparePriority);
 			// From slowest to fastest
-			for (let i of dancers) {
+			for (let i of dancers)
+				if (!dancers[i])  this.runMove(baseMove.id, 'debug', 0, this.getAbility('dancer'), undefined, true);
 				this.faintMessages();
 				this.add('-activate', dancers[i], 'ability: Dancer');
 				this.runMove(baseMove.id, dancers[i], 0, this.getAbility('dancer'), undefined, true);
