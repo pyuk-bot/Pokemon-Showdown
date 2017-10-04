23402bfbdb60ed5afdc791e808d336f85bb9eb0f
MacChaeger <msaimrkon@gmail.com>
2017-10-03 23:37:35 -0500

    Actually target the ally I think?

--- test/simulator/abilities/dancer.js
+++ test/simulator/abilities/dancer.js
@@ -66,8 +66,8 @@ describe('Dancer', function () {
 		p1.active[0].boostBy({atk: 5, spe: 6});
 		p2.active[0].boostBy({atk: -6});
 		p2.active[1].boostBy({spe: 6});
-		p1.chooseMove(1).chooseMove(2, 1);
-		p2.chooseMove(2, 2).chooseMove(1, 1);
+		p1.chooseMove(1).chooseMove(1, 1);
+		p2.chooseMove(1, -2).chooseMove(1, 1);
 		assert.fullHP(p2.active[0]);
 		assert.statStage(p1.active[0], 'atk', 6);
 		assert.statStage(p1.active[0], 'spe', 6);
