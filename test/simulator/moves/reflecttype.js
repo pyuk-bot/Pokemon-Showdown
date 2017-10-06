4f2950c2c6e87bfd4f3296e4b9aad82bea3d4ec2
MacChaeger <msaimrkon@gmail.com>
2017-10-05 19:29:11 -0500

    Add test for ???/Secondary/Added

--- test/simulator/moves/reflecttype.js
+++ test/simulator/moves/reflecttype.js
@@ -28,7 +28,7 @@ describe('Reflect Type', function () {
 		assert.strictEqual(battle.p2.active[0].getTypes()[0], 'Flying');
 	});
 
-	it('should turn the "???" type into "Normal" when used against a Pokemon whose type contains "???" and an added type', function () {
+	it('should turn the "???" type into "Normal" when used against a Pokemon whose type is only "???" and an added type', function () {
 		battle = common.createBattle();
 		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Arcanine', ability: 'intimidate', moves: ['burnup']}]);
@@ -36,5 +36,15 @@ describe('Reflect Type', function () {
 		battle.commitDecisions();
 		assert.strictEqual(battle.p2.active[0].getTypes().length, 2);
 		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Normal');
+	});
+
+	it('should ignore the "???" type when used against a Pokemon whose type contains "???", an added type, and another type', function () {
+		battle = common.createBattle();
+		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Moltres', ability: 'pressure', moves: ['burnup']}]);
+		p1.chooseMove(2, 1).foe.chooseDefault();
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].getTypes().length, 2);
+		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Flying');
 	});
 });
