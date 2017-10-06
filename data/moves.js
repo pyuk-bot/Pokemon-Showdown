98b8c9eeef4cfd6d6b20f9a2f77899e180223d9c
MacChaeger <msaimrkon@gmail.com>
2017-10-05 19:16:02 -0500

    More sensible boolean structure

--- data/moves.js
+++ data/moves.js
@@ -13038,15 +13038,18 @@ exports.BattleMovedex = {
 		flags: {protect: 1, authentic: 1, mystery: 1},
 		onHit: function (target, source) {
 			if (source.template && (source.template.num === 493 || source.template.num === 773)) return false;
-			if (target.getTypes().length === 1 && target.getTypes()[0] === '???') return false;
-			this.add('-start', source, 'typechange', '[from] move: Reflect Type', '[of] ' + target);
-			if (target.addedType && target.getTypes(true).length === 1) {
-				source.types = target.getTypes(true).map(type => type === '???' ? 'Normal' : type);
-			} else {
-				source.types = target.getTypes(true).filter(type => type !== '???');
+			let newBaseTypes = target.getTypes(true).filter(type => type !== '???');
+			if (!newBaseTypes.length) {
+				if (target.addedType) {
+					newBaseTypes = ['Normal'];
+				} else {
+					return false;
+				}
 			}
+			source.types = newBaseTypes;
 			source.addedType = target.addedType;
 			source.knownType = target.side === source.side && target.knownType;
+			this.add('-start', source, 'typechange', '[from] move: Reflect Type', '[of] ' + target);
 		},
 		secondary: false,
 		target: "normal",
