5261ec05a58c8d0876a2374af658cc5018b43846
MacChaeger <msaimrkon@gmail.com>
2017-10-05 19:48:07 -0500

    Simplify tests

--- test/simulator/moves/reflecttype.js
+++ test/simulator/moves/reflecttype.js
@@ -21,11 +21,16 @@ describe('Reflect Type', function () {
 
 	it('should ignore the "???" type when used against a Pokemon whose type contains "???" and a non-added type', function () {
 		battle = common.createBattle();
-		battle.join('p1', 'Guest 1', 1, [{species: 'Moltres', ability: 'pressure', moves: ['burnup']}]);
-		battle.join('p2', 'Guest 2', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype']}]);
+		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Moltres', ability: 'intimidate', moves: ['burnup']}]);
 		battle.commitDecisions();
 		assert.strictEqual(battle.p2.active[0].getTypes().length, 1);
 		assert.strictEqual(battle.p2.active[0].getTypes()[0], 'Flying');
+		p1.chooseMove(2, 1).foe.chooseDefault();
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].getTypes().length, 2);
+		assert.strictEqual(battle.p2.active[0].getTypes()[0], 'Flying');
+		assert.strictEqual(battle.p2.active[0].getTypes()[1], 'Ghost');
 	});
 
 	it('should turn the "???" type into "Normal" when used against a Pokemon whose type is only "???" and an added type', function () {
@@ -36,17 +41,6 @@ describe('Reflect Type', function () {
 		battle.commitDecisions();
 		assert.strictEqual(battle.p1.active[0].getTypes().length, 2);
 		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Normal');
-		assert.strictEqual(battle.p1.active[0].getTypes()[1], 'Ghost');
-	});
-
-	it('should ignore the "???" type when used against a Pokemon whose type contains "???", an added type, and another type', function () {
-		battle = common.createBattle();
-		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
-		battle.join('p2', 'Guest 2', 1, [{species: 'Moltres', ability: 'pressure', moves: ['burnup']}]);
-		p1.chooseMove(2, 1).foe.chooseDefault();
-		battle.commitDecisions();
-		assert.strictEqual(battle.p1.active[0].getTypes().length, 2);
-		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Flying');
 		assert.strictEqual(battle.p1.active[0].getTypes()[1], 'Ghost');
 	});
 });
