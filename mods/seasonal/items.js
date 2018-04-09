9483c54b88fff453e2301d49e7f165dae4a9020d
MacChaeger <msaimrkon@gmail.com>
2018-04-08 23:43:07 -0500

    Add a fuck ton of mons and fix a fuck ton more

--- mods/seasonal/items.js
+++ mods/seasonal/items.js
@@ -10,6 +10,16 @@ exports.BattleItems = {
 		zMoveUser: ["Skarmory"],
 		gen: 7,
 		desc: "If held by imas with Accele Squawk, it can use B O I.",
+	}, 
+	"tiksiumz": {
+		id: "tiksiumz",
+		name: "Tiksium Z",
+		onTakeItem: false,
+		zMove: "Devolution Wave",
+		zMoveFrom: "Rock Slide",
+		zMoveUser: ["Cradily"],
+		gen: 7,
+		desc: "If held by Tiksi with Rock Slide, it can use Devolution Wave.",
 	},
 	"tiksiumz": {
 		id: "tiksiumz",
